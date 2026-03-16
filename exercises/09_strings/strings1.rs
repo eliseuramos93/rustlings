f7c1070bca4d3363f007dd7b1b5ab0705c88735f
Eliseu Ramos <eliseu.mec@gmail.com>
2026-03-16 19:40:12 -0300

    09_strings completed

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -1,6 +1,7 @@
 // TODO: Fix the compiler error without changing the function signature.
 fn current_favorite_color() -> String {
-    "blue"
+    // "blue".to_string() => this is considered conversion of a slice into a string. crzy rustaceans
+    String::from("blue")
 }
 
 fn main() {
